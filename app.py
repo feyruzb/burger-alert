9f469021eb55837fcdd692e7c58ffbe83ecd3c50
Baghirov Feyruz <feyruzbaghirov@gmail.com>
2025-07-31 18:10:25 +0200

    add car distribution system

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask , render_template, request
 from flask_sqlalchemy import SQLAlchemy
-import orders
 from datetime import datetime, date, time
+from typing import Dict, List
+import random
 
 from datetime import datetime
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///orders.db'
 db = SQLAlchemy(app)
-requests = orders.Orders()
 
 class Orders(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -18,18 +18,6 @@ class Orders(db.Model):
 
     def __repr__(self):
         return '<Order %r>' % self.id
-
-@app.route("/today_orders")
-def return_todays_orders():
-    start_of_today = datetime.combine(date.today(), time.min)
-    end_of_today = datetime.combine(date.today(), time.max)
-
-    orders = Orders.query.filter(
-        Orders.date_created >= start_of_today,
-        Orders.date_created <= end_of_today
-    ).order_by(Orders.date_created).all()
-
-    return render_template("today_orders.html", orders=orders)
 
 @app.route("/")
 def index_page():
@@ -54,6 +42,56 @@ def submit():
                             order=order)
     except:
         return render_template("failed.html")
+
+@app.route("/today_orders")
+def return_todays_orders():
+    start_of_today = datetime.combine(date.today(), time.min)
+    end_of_today = datetime.combine(date.today(), time.max)
+
+    orders = Orders.query.filter(
+        Orders.date_created >= start_of_today,
+        Orders.date_created <= end_of_today
+    ).order_by(Orders.date_created).all()
+
+    return render_template("today_orders.html", orders=orders)
+
+@app.route("/car_distribution")
+def return_car_distribution():
+    start_of_today = datetime.combine(date.today(), time.min)
+    end_of_today = datetime.combine(date.today(), time.max)
+
+
+    people_with_cars = list(set([ order.name for order in Orders.query.filter(
+        Orders.date_created >= start_of_today,
+        Orders.date_created <= end_of_today,
+        Orders.mode =="Has a car and will drive people from office"
+    ).order_by(Orders.name).all() ]))
+
+    people_without_cars = list(set([ order.name for order in Orders.query.filter(
+        Orders.date_created >= start_of_today,
+        Orders.date_created <= end_of_today,
+        Orders.mode =="Want to be driven by someone from office"
+    ).order_by(Orders.name).all()]))
+    # print(people_with_cars)
+    # print(people_without_cars)
+
+    # random.shuffle(people_without_cars)
+
+    list_of_distributes: Dict[str, List[str]] = {}
+
+    for driver in people_with_cars:
+        list_of_distributes[driver] = list()
+
+    driver_cnt = 0
+    for walker in people_without_cars:
+        list_of_distributes[people_with_cars[driver_cnt]].append(walker)
+        driver_cnt = (driver_cnt + 1) % len(people_with_cars)
+
+    print("list of distributors", list_of_distributes.items())
+    list_of_distributes = list(list_of_distributes.items())
+
+
+    return render_template("car_distribution.html", list_of_distributes=list_of_distributes)
 
 @app.route('/hello')
 def hello():
