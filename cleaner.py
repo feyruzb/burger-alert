354f0d41c5194026b8fc091976eb879602c6b138
Baghirov Feyruz <rww1d4@inf.elte.hu>
2025-11-04 02:41:16 +0100

    added automatic database cleaner

---
+++ cleaner.py
@@ -0,0 +1,11 @@
+"""
+Database cleaner module
+"""
+from pathlib import Path
+from sqlalchemy import create_engine, text
+
+engine = create_engine('sqlite:///instance/orders.db')
+
+with engine.begin() as conn:
+    conn.execute(text("DELETE FROM orders"))
+    print("All rows deleted.")
